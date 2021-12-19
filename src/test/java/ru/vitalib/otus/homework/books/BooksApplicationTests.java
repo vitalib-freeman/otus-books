a733e85eac57897c4335bac13b5a8387dd80fa06
Vitali Baranov <vitalib.freeman@gmail.com>
2021-12-19 11:58:28 +0300

    Initial commit

---
+++ src/test/java/ru/vitalib/otus/homework/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitalib.otus.homework.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
