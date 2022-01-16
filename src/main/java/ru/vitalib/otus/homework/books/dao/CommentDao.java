90effb7d872dcbc04d6614aeb912787c490f6ef8
Vitali Baranov <vitalib.freeman@gmail.com>
2022-01-16 21:00:16 +0300

    spring_data modify comment dao

--- src/main/java/ru/vitalib/otus/homework/books/dao/CommentDao.java
+++ src/main/java/ru/vitalib/otus/homework/books/dao/CommentDao.java
@@ -1,9 +1,7 @@
 package ru.vitalib.otus.homework.books.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.vitalib.otus.homework.books.domain.Comment;
 
-public interface CommentDao {
-  Comment save(Comment comment);
-  void delete(Comment comment);
-  Comment findById(Long id);
+public interface CommentDao extends JpaRepository<Comment, Long> {
 }
